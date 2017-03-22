0404aa8ae5798d432413af8531abaa3a97167f67
asbonato <asbonato@gmail.com>
2017-03-22 20:37:30 -0300

    Refactoring com padrão DAO

--- arqdesis17_aula04/src/model/Cliente.java
+++ arqdesis17_aula04/src/model/Cliente.java
@@ -1,118 +1,51 @@
 package model;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import dao.ClienteDAO;
 
 public class Cliente {
 	private int id;
 	private String nome;
 	private String fone;
 	private String email;
+	private ClienteDAO dao;
 
-	static {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			throw new RuntimeException(e);
-		}
-	}
+	
 	
 	public Cliente(){
-		
+		dao = new ClienteDAO();
 	}
-
 
 	public Cliente(int id, String nome, String fone, String email) {
 		this.id = id;
 		this.nome = nome;
 		this.fone = fone;
 		this.email = email;
-	}
-
-
-	
-	// Obtém conexão com o banco de dados
-	public Connection obtemConexao() throws SQLException {
-		return DriverManager
-				.getConnection("jdbc:mysql://localhost/vendas?user=alunos&password=alunos");
+		dao = new ClienteDAO();
 	}
 
 	public void criar() {
-		String sqlInsert = "INSERT INTO cliente(nome, fone, email) VALUES (?, ?, ?)";
-		// usando o try with resources do Java 7, que fecha o que abriu
-		try (Connection conn = obtemConexao();
-				PreparedStatement stm = conn.prepareStatement(sqlInsert);) {
-			stm.setString(1, getNome());
-			stm.setString(2, getFone());
-			stm.setString(3, getEmail());
-			stm.execute();
-			String sqlQuery  = "SELECT LAST_INSERT_ID()";
-			try(PreparedStatement stm2 = conn.prepareStatement(sqlQuery);
-				ResultSet rs = stm2.executeQuery();) {
-				if(rs.next()){
-					setId(rs.getInt(1));
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		dao.criar(this);
 	}
 
 	public void atualizar() {
-		String sqlUpdate = "UPDATE cliente SET nome=?, fone=?, email=? WHERE id=?";
-		// usando o try with resources do Java 7, que fecha o que abriu
-		try (Connection conn = obtemConexao();
-				PreparedStatement stm = conn.prepareStatement(sqlUpdate);) {
-			stm.setString(1, getNome());
-			stm.setString(2, getFone());
-			stm.setString(3, getEmail());
-			stm.setInt(4, getId());
-			stm.execute();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		dao.atualizar(this);
 	}
 
 	public void excluir() {
-		String sqlDelete = "DELETE FROM cliente WHERE id = ?";
-		// usando o try with resources do Java 7, que fecha o que abriu
-		try (Connection conn = obtemConexao();
-				PreparedStatement stm = conn.prepareStatement(sqlDelete);) {
-			stm.setInt(1, getId());
-			stm.execute();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		dao.excluir(this);
 	}
 
 	public void carregar() {
-		String sqlSelect = "SELECT nome, fone, email FROM cliente WHERE cliente.id = ?";
-		// usando o try with resources do Java 7, que fecha o que abriu
-		try (Connection conn = obtemConexao();
-				PreparedStatement stm = conn.prepareStatement(sqlSelect);) {
-			stm.setInt(1, getId());
-			try (ResultSet rs = stm.executeQuery();) {
-				if (rs.next()) {
-					setNome(rs.getString("nome"));
-					setFone(rs.getString("fone"));
-					setEmail(rs.getString("email"));
-				} else {
-					setId(-1);
-					setNome(null);
-					setFone(null);
-					setEmail(null);
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		} catch (SQLException e1) {
-			System.out.print(e1.getStackTrace());
-		}
+		Cliente cliente = dao.carregar(id);
+		setNome(cliente.getNome());
+		setFone(cliente.getFone());
+		setEmail(cliente.getEmail());
+		setId(cliente.getId());
 	}
 
 
